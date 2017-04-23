880087a9c29eac25e284fd54effc8268360e1852
Daniel Diaz <diaz.carrete@facebook.com>
2017-04-23 22:15:14 +0200

    Test program.

--- src/test/java/info/danidiaz/util/Main.java
+++ src/test/java/info/danidiaz/util/Main.java
@@ -1,9 +1,10 @@
 package info.danidiaz.util;
 
+import java.util.ListIterator;
+
 public class Main {
 
     public static void main(String[] args) {
-
         DoublyLinkedList<Integer> l = new DoublyLinkedList<>();
         l.addFirst(0);
         l.addFirst(1);
@@ -14,6 +15,17 @@ public class Main {
         l.addLast(0);
         l.addLast(1);
         l.addLast(2);
+        System.out.println(l);
+        System.out.println(l.size());
+        l = new DoublyLinkedList<>();
+        l.addFirst(0);
+        l.addFirst(1);
+        l.addFirst(2);
+        ListIterator<Integer> iter = l.listIterator();
+        iter.next();
+        iter.next();
+        iter.remove();
+        iter.add(11);
         System.out.println(l);
         System.out.println(l.size());
     }
